ffef3ff86bd5cb3b383d512119e0cf4cd599ab63
qh4r <qhr666@gmail.com>
2017-04-27 08:39:15 +0200

    Update js_misc.js

--- js_misc.js
+++ js_misc.js
@@ -4,3 +4,14 @@ String.prototype.odd = function () {
  return Array.prototype.slice.call(this)
   .reduce((s, e, i) => i % 2 ? s + e : s)
 }
+
+
+// blok catch nie jest konieczny try finally tez uchroni nas przed bledem
+function test(x){
+ try{var res = 10/x
+ } finally {
+  return res
+ }
+}
+test({data: 5}) //2
+test() // undefined
